0af41c58db0aba6d83f4d360cf6563a86ec0644f
Thịnh Trần <thinhtv@uniorbit.org>
2024-10-23 11:19:12 +0700

    test port

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,12 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  
+  // Sử dụng PORT từ biến môi trường hoặc một giá trị mặc định
+  const port = process.env.PORT || 3000;
+
+  // Lắng nghe trên tất cả các địa chỉ IP (0.0.0.0) để có thể truy cập từ bên ngoài
+  await app.listen(port, '0.0.0.0');
+  console.log(`Application is running on: ${await app.getUrl()}`);
 }
 bootstrap();

112de63ee17222407689136c89e6ace98efbeece
Thịnh Trần <thinhtv@uniorbit.org>
2024-10-23 11:54:03 +0700

    change link BE

--- src/main.ts
+++ src/main.ts
@@ -3,12 +3,6 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  
-  // Sử dụng PORT từ biến môi trường hoặc một giá trị mặc định
-  const port = process.env.PORT || 3000;
-
-  // Lắng nghe trên tất cả các địa chỉ IP (0.0.0.0) để có thể truy cập từ bên ngoài
-  await app.listen(port, '0.0.0.0');
-  console.log(`Application is running on: ${await app.getUrl()}`);
+  await app.listen('https://camera-fj1.onrender.com/');
 }
 bootstrap();

743923f12558e39486a93b12bb426a5a0886a35b
Thịnh Trần <thinhtv@uniorbit.org>
2024-10-28 14:04:07 +0700

    change port

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  const port = process.env.PORT || 8080;
+  const port = process.env.PORT || 3000;
   console.log('port',port);
 
   await app.listen(port, '0.0.0.0');
